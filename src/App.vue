f2b423470cdc27dc9c31bcce91b224f0929f5b70
Javier Pestana <javier.pestana85@gmail.com>
2024-09-21 14:18:23 +0100

    setup Vue app, Form component and styles

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Form from "./components/Form.vue";
+</script>
+
+<template>
+  <h1 class="titulo">ClimaCheck</h1>
+  <div class="contenedor buscador-clima">
+    <Form />
+  </div>
+</template>
---
+++ src/components/Form.vue
@@ -0,0 +1,30 @@
+<script setup>
+const countries = [
+  { code: "CO", name: "Colombia" },
+  { code: "CR", name: "Costa Rica" },
+  { code: "US", name: "Estados Unidos" },
+  { code: "ES", name: "España" },
+  { code: "PE", name: "Perú" },
+  { code: "PT", name: "Portugal" },
+  { code: "VE", name: "Venezuela" },
+];
+</script>
+
+<template>
+  <form class="formulario">
+    <div class="campo">
+      <label for="city">City</label>
+      <input type="text" id="city" placeholder="City" />
+    </div>
+    <div class="campo">
+      <label for="country">Country</label>
+      <select id="country">
+        <option value="">-- Select Country --</option>
+        <option v-for="country in countries" :value="country.code">
+          {{ country.name }}
+        </option>
+      </select>
+      <input type="submit" value="Check Weather" />
+    </div>
+  </form>
+</template>
