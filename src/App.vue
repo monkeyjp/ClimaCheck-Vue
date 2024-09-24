6a413c3fc8c6ab84f0a42d120470b9712ff92bdf
Javier Pestana <javier.pestana85@gmail.com>
2024-09-24 14:59:09 +0100

    adding Spinner and Weather components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script setup>
 import Form from "./components/Form.vue";
+import Weather from "./components/Weather.vue";
 import useWeather from "./composables/useWeather";
+import Spinner from "./components/Spinner.vue";
 
-const { getWeather } = useWeather();
+const { getWeather, weather, showWeather, loading } = useWeather();
 </script>
 
 <template>
   <h1 class="titulo">ClimaCheck</h1>
   <div class="contenedor buscador-clima">
     <Form @get-weather="getWeather" />
+    <Spinner v-if="loading" />
+    <Weather v-if="showWeather" :weather="weather" />
   </div>
 </template>
--- src/composables/useWeather.js
+++ src/composables/useWeather.js
@@ -1,25 +1,39 @@
+import { ref, computed } from "vue"
 import axios from "axios"
 export default function useWeather() {
+
+    const weather = ref({})
+
+    const loading = ref(false)
+
     const getWeather = async ({ city, country }) => {
         const key = import.meta.env.VITE_API_KEY
-
+        loading.value = true
+        weather.value = {}
         try {
             const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&limit=1&appid=${key}`
             const { data } = await axios(url)
             const { lat, lon } = data[0]
-            const urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`
+            const urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`
             const { data: result } = await axios(urlWeather)
-            console.log(result);
-
-
-
-
+            weather.value = result
         } catch (error) {
+            console.log(error);
 
+        } finally {
+            loading.value = false
         }
 
     }
+
+    const showWeather = computed(() => {
+        return Object.values(weather.value).length > 0
+    })
+
     return {
-        getWeather
+        getWeather,
+        weather,
+        showWeather,
+        loading
     }
 }
